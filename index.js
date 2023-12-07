0e53817c101959e940c248ef5ccab0b4884e8181
OlessyaOG <152127554+OlessyaOG@users.noreply.github.com>
2023-12-06 21:29:20 -0800

    App weather image for UI
    created a function to ensure that the relevant icon appears (depending on condition)

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ function refreshWeather(response) {
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
 
+  let iconElement = document.querySelector("#icon");
+
   //Replacing inside the UI
+
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon">`;
 }
 
 // Function to format day of the week
