0d22fd19d0a56f6f022529ab28bd6d7cc8fa6ce4
OlessyaOG <152127554+OlessyaOG@users.noreply.github.com>
2023-12-06 18:14:21 -0800

    added API
    Added search functionality and API

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
+//New function that will receive response and will update weather data points for a particular city
+
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+//New function that will search the city.
+//Make api call and update UI
+
+function searchCity(city) {
+  let apiKey = "ad7tba5a453o9ae4007ed34f61a4b5a8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 // Getting the city value and replacing it with the existing city on the page
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
 
   //Call for API and search for the city we have
-
-  
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Tokyo")
